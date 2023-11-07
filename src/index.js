63ec13fef3fa7e686d94f767f44b007fd570a6d5
Bhupinder Singh <bhupinder.singh1983@gmail.com>
2023-11-07 18:01:02 -0500

    First Github commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './HelloWorld.js'
+
+ReactDOM.render(
+    <div><HelloWorld/> </div>,
+    document.getElementById("container")
+)
